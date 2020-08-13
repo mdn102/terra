0033e6fb7d497e9b052eb7e0945d91cfc99c14ee
Noa Kim <noanonoa@gmail.com>
2020-08-12 20:21:09 -0700

    home page style finish

--- terra/components/UserInterface.js
+++ terra/components/UserInterface.js
@@ -5,60 +5,58 @@ import Router from 'next/router';
 const userInterface = () => {
   return (
     <div className="center">
-      <br />
       <h2>Where would you like to go?</h2>
-      <br />
       <div className="layout-container">
         <Row>
           <Col>
-            <div>
-              <button onClick={() => Router.push('/beach')}><img src="/img/beach/beach.png" alt="beach image"/></button>
-              <p>Beach</p>
-            </div>
-            <br /><br /><br /><br /><br />
-            <div>
-              <button onClick={() => Router.push('/city')}><img src="/img/city/city.png" /></button>
-              <p>City</p>
-            </div>
-            <br /><br /><br /><br /><br />
-            <div>
-              <button onClick={() => Router.push('/desert')}><img src="/img/desert/desert.png" /></button>
-              <p>Desert</p>
-            </div>
-          </Col>
-          <Col xs={6}>
-            <Jumbotron>
-              <div className="jumbo">
-                <div className="jumbo-content">
-                  <h1>Welcome to Terra</h1>
-                  <p>
-                    This is a simple environmental application, built to help
-                    educate the youth
-                    <br />
-                    about the importance of how humans can negatively impact our
-                    planet.
-                  </p>
-                  <p>
-                    <Button variant="primary">Learn more</Button>
-                  </p>
-                </div>
+            <div className="left-btn-container">
+              <div>
+                <button className="img-btn" onClick={() => Router.push('/beach')}><img src="/img/beach/beach.png" alt="beach image"/></button>
+                <p>Beach</p>
               </div>
-            </Jumbotron>
+              <div>
+                <button className="img-btn" onClick={() => Router.push('/city')}><img src="/img/city/city.png" /></button>
+                <p>City</p>
+              </div>
+              <div>
+                <button className="img-btn" onClick={() => Router.push('/desert')}><img src="/img/desert/desert.png" /></button>
+                <p>Desert</p>
+              </div>
+            </div>
           </Col>
           <Col>
-            <div>
-              <button onClick={() => Router.push('/jungle')}><img src="/img/jungle/jungle.png" /></button>
-              <p>Jungle</p>
-            </div>
-            <br /><br /><br /><br /><br />
-            <div>
-              <button onClick={() => Router.push('/ocean')}><img src="/img/ocean/ocean.png" /></button>
-              <p>Ocean</p>
-            </div>
-            <br /><br /><br /><br /><br />
-            <div>
-              <button onClick={() => Router.push('/forest')}><img src="/img/forest/forest.png" /></button>
-              <p>Forest</p>
+              <div className="jumbo">
+                <Jumbotron>
+                  <div className="jumbo-content">
+                    <h1>Welcome to Terra</h1>
+                    <p>
+                      This is a simple environmental application, built to help
+                      educate the youth
+                      <br />
+                      about the importance of how humans can negatively impact our
+                      planet.
+                    </p>
+                    <p>
+                      <Button variant="primary">Learn more</Button>
+                    </p>
+                  </div>
+                </Jumbotron>
+              </div>
+          </Col>
+          <Col>
+            <div className="right-btn-container">
+              <div>
+                <button className="img-btn" onClick={() => Router.push('/jungle')}><img src="/img/jungle/jungle.png" /></button>
+                <p>Jungle</p>
+              </div>
+              <div>
+                <button className="img-btn" onClick={() => Router.push('/ocean')}><img src="/img/ocean/ocean.png" /></button>
+                <p>Ocean</p>
+              </div>
+              <div>
+                <button className="img-btn" onClick={() => Router.push('/forest')}><img src="/img/forest/forest.png" /></button>
+                <p>Forest</p>
+              </div>
             </div>
           </Col>
         </Row>
@@ -67,18 +65,41 @@ const userInterface = () => {
         .center {
           text-align: center;
         }
-        .jumbo {
-          width: 1112px;
-          height: 700px;
-        }
-        .jumbo-content {
-          position: relative;
-          right: 135px;
-          top: 300px;
-        }
         .layout-container {
+          min-width: 900px;
+          max-width: 1920px;
+          min-height: 500px;
+          max-height: 800px;
           display: flex;
+          justify-content: space-around;
+        }
+        .left-btn-container {
+          display: flex;
+          flex-direction: column;
+          justify-content: space-around;
+          height: 800px;
+        }
+        .jumbo {
+          height: 800px;
+          width: 1100px;
+          display: flex;
+          align-items: center;
           justify-content: center;
+          border: 2px solid black;
+          margin-left: 100px;
+          margin-right: 100px;
+        }
+        .right-btn-container {
+          display: flex;
+          flex-direction: column;
+          justify-content: space-around;
+          height: 800px;
+        }
+        .img-btn {
+          border-style: none;
+          padding: 0;
+          margin: 0;
+          border-radius: 15px;
         }
       `}</style>
     </div>
